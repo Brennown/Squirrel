88ea050295fcdc4e98bbb166bdebe8c280193447
Brenno Caldas <brennodesigner100@gmail.com>
2020-11-28 10:09:02 -0300

    Creating The Project Squirrel

---
+++ SquirrelProject/SquirrelUi/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SquirrelUi.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
